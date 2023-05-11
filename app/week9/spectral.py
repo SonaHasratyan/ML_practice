f4074041f72a07bf1324b2a775661562c78270a2
Sona Hasratyan <sonah@10web.io>
2023-05-11 19:44:57 +0400

    spectral's initial commit

---
+++ app/week9/spectral.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.datasets import make_moons
+
+
+class SpectralClustering:
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        pass
+
+    def predict(self, X_test):
+        pass
