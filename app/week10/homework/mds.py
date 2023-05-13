c3650923944a2e5d57167c141076ca3b6ae43f0d
Sona Hasratyan <sonah@10web.io>
2023-05-13 22:38:46 +0400

    week10

---
+++ app/week10/homework/mds.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class MDS:
+    def __init__(self, n_components=2, n_init=4, max_iter=300, eps=0.001):
+        pass
+
+    def fit(self, X):
+        pass
+
+    def fit_transform(self):
+        pass
---
+++ app/week10/homework/t_sne.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class TSNE:
+    def __init__(self, n_components=2, elearning_rate='auto', n_iter=1000, min_grad_norm=1e-07):
+        pass
+
+    def fit(self, X):
+        pass
+
+    def fit_transform(self):
+        pass
