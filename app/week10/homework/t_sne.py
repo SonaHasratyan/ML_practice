48d8c7f629d54ed6c9d7998cde05f66c90a8ac4e
Sona Hasratyan <sonah@10web.io>
2023-05-20 01:21:25 +0400

    adding sigma selection logic

--- app/week10/homework/t_sne.py
+++ app/week10/homework/t_sne.py
@@ -1,9 +1,10 @@
 import numpy as np
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
+from sklearn.manifold._t_sne import TSNE
 
 
-class TSNE:
+class _TSNE:
     def __init__(
         self,
         n_components=2,
@@ -20,8 +21,8 @@ class TSNE:
 
         self.X = None
         self.y = None
+        self.prev_y = None
         self.m = 0
-        self.variances = None
         self.affinity = None  # p_{ij}
         self.similarity = None  # q_{ij}
 
@@ -30,22 +31,21 @@ class TSNE:
     def fit(self, X):
         self.X = X
         self.m = self.X.shape[0]
-        self.__compute_variances()
         self.__compute_affinity()
         self.__init_y()
 
     def fit_transform(self, X):
         self.X = X
         self.m = self.X.shape[0]
-        self.__compute_variances()
         self.__compute_affinity()
         self.__init_y()
 
         for i in range(self.n_iter):
             self.__compute_similarity()
             grad = self.__compute_grad()
-            # todo v
-            self.y = self.y + self.momentum * grad + self.learning_rate * (self.y)
+            prev_y = self.y.copy()
+            self.y = self.y + self.momentum * grad + self.learning_rate * (self.y - self.prev_y)
+            self.prev_y = prev_y
 
     def __compute_affinity(self):
         self.affinity = np.zeros((self.m, self.m))
@@ -54,11 +54,10 @@ class TSNE:
                 if i == j:
                     continue
 
-                self.affinity[i][j] += np.exp(
-                    -(np.linalg.norm(self.X[i] - self.X[j]) ** 2)
-                    / (2 * self.variances[i][j] ** 2)
-                )
+                self.affinity[i][j] = -(np.linalg.norm(self.X[i] - self.X[j]) ** 2)
 
+            sigma_i = self.__binary_search_sigma_i(self.affinity[i])
+            self.affinity[i] = np.exp(self.affinity[i] / (2 * sigma_i ** 2))
             self.affinity[i] /= sum(self.affinity[i])
 
         for i in range(self.m):
@@ -70,13 +69,32 @@ class TSNE:
                 )
                 self.affinity[j][i] = self.affinity[i][j]
 
-    def __compute_variances(self):
-        # todo v
-        self.variances = np.full((self.m, self.m), fill_value=self.perplexity)
+    @staticmethod
+    def __perp(affinity):
+        return 2 ** (-sum(affinity * np.log2(affinity)))
+
+    def __binary_search_sigma_i(self, row_i, lower_bound=1e-10, upper_bound=1e10, eps=1e-8):
+
+        tmp_sigma = (lower_bound + upper_bound) / 2
+
+        affinity = np.exp(row_i / (2 * tmp_sigma ** 2))
+        affinity /= sum(affinity)
+
+        perp = self.__perp(affinity)
+        if perp > self.perplexity:
+            upper_bound = tmp_sigma
+        else:
+            lower_bound = tmp_sigma
+
+        if np.abs(perp - self.perplexity) <= eps:
+            return tmp_sigma
+        
+        return self.__binary_search_sigma_i(row_i, lower_bound, upper_bound, eps)
 
     def __init_y(self):
         # todo v: * np.eye?
         self.y = np.random.normal(loc=0.0, scale=10 ** (-4), size=self.m)
+        self.prev_y = np.zeros(self.m)
 
     def __compute_similarity(self):
         self.similarity = np.zeros((self.m, self.m))
@@ -108,15 +126,15 @@ class TSNE:
 # --------------BLOBS--------------
 M = 100
 X_blobs, y_blobs = make_blobs(
-    M, n_features=4, random_state=78, cluster_std=0.6, centers=4
+    M, n_features=3, random_state=78, cluster_std=0.6, centers=3
 )
 plt.figure(figsize=(16, 8))
 plt.scatter(X_blobs[:, 0], X_blobs[:, 1], c=y_blobs)
 plt.show()
 
-tsne = TSNE(n_components=4)
-tsne.fit_transform(X_blobs)
-_y_blobs_pred = tsne.y
+_tsne = _TSNE(n_components=3)
+_tsne.fit_transform(X_blobs)
+_y_blobs_pred = _tsne.y
 
 plt.figure(figsize=(16, 8))
 plt.scatter(X_blobs[:, 0], X_blobs[:, 1], c=_y_blobs_pred)
