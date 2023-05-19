d799d24d6a20fae0bf7ce7337db45e532c93e994
Sona Hasratyan <sonah@10web.io>
2023-05-20 02:15:49 +0400

    further improvements

--- app/week10/homework/t_sne.py
+++ app/week10/homework/t_sne.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
-from sklearn.manifold._t_sne import TSNE
 
 
 class _TSNE:
@@ -44,7 +43,11 @@ class _TSNE:
             self.__compute_similarity()
             grad = self.__compute_grad()
             prev_y = self.y.copy()
-            self.y = self.y + self.momentum * grad + self.learning_rate * (self.y - self.prev_y)
+            self.y = (
+                self.y
+                + self.momentum * grad
+                + self.learning_rate * (self.y - self.prev_y)
+            )
             self.prev_y = prev_y
 
     def __compute_affinity(self):
@@ -57,15 +60,15 @@ class _TSNE:
                 self.affinity[i][j] = -(np.linalg.norm(self.X[i] - self.X[j]) ** 2)
 
             sigma_i = self.__binary_search_sigma_i(self.affinity[i])
-            self.affinity[i] = np.exp(self.affinity[i] / (2 * sigma_i ** 2))
+            self.affinity[i] = np.exp(self.affinity[i] / (2 * (sigma_i**2)))
             self.affinity[i] /= sum(self.affinity[i])
 
         for i in range(self.m):
             for _j in range(self.m - i):
                 j = _j + i
 
-                self.affinity[i][j] = (
-                    (self.affinity[i][j] + self.affinity[j][i]) / 2 * self.m
+                self.affinity[i][j] = (self.affinity[i][j] + self.affinity[j][i]) / (
+                    2 * self.m
                 )
                 self.affinity[j][i] = self.affinity[i][j]
 
@@ -73,14 +76,19 @@ class _TSNE:
     def __perp(affinity):
         return 2 ** (-sum(affinity * np.log2(affinity)))
 
-    def __binary_search_sigma_i(self, row_i, lower_bound=1e-10, upper_bound=1e10, eps=1e-8):
-
+    def __binary_search_sigma_i(
+        self, row_i, lower_bound=1e-10, upper_bound=1e10, eps=1e-8
+    ):
         tmp_sigma = (lower_bound + upper_bound) / 2
-
-        affinity = np.exp(row_i / (2 * tmp_sigma ** 2))
+        affinity = np.exp(row_i / (2 * (tmp_sigma**2)))
         affinity /= sum(affinity)
 
         perp = self.__perp(affinity)
+
+        # todo: close v
+        if np.isnan(perp):
+            print("SH")
+
         if perp > self.perplexity:
             upper_bound = tmp_sigma
         else:
@@ -88,7 +96,7 @@ class _TSNE:
 
         if np.abs(perp - self.perplexity) <= eps:
             return tmp_sigma
-        
+
         return self.__binary_search_sigma_i(row_i, lower_bound, upper_bound, eps)
 
     def __init_y(self):
@@ -103,9 +111,9 @@ class _TSNE:
             for _j in range(self.m - i):
                 j = _j + i
 
-                self.similarity[i][j] += np.exp(
-                    -(np.linalg.norm(self.y[i] - self.y[j]) ** 2)
-                )
+                self.similarity[i][j] = (
+                    1 + (np.linalg.norm(self.y[i] - self.y[j]) ** 2)
+                ) ** (-1)
                 self.similarity[j][i] = self.similarity[i][j]
 
             self.similarity[i] /= sum(self.similarity[i])
@@ -126,13 +134,13 @@ class _TSNE:
 # --------------BLOBS--------------
 M = 100
 X_blobs, y_blobs = make_blobs(
-    M, n_features=3, random_state=78, cluster_std=0.6, centers=3
+    M, n_features=4, random_state=78, cluster_std=0.6, centers=4
 )
 plt.figure(figsize=(16, 8))
 plt.scatter(X_blobs[:, 0], X_blobs[:, 1], c=y_blobs)
 plt.show()
 
-_tsne = _TSNE(n_components=3)
+_tsne = _TSNE(n_components=4)
 _tsne.fit_transform(X_blobs)
 _y_blobs_pred = _tsne.y
 
