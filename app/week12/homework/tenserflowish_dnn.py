86d88375075f4c76da347d12f93deb1cacebe7b1
Sona Hasratyan <sonah@10web.io>
2023-05-31 20:56:48 +0400

    week12 initial commit

---
+++ app/week12/homework/tenserflowish_dnn.py
@@ -0,0 +1,11 @@
+"""
+     You need to do the same implementation of the previous homework but with tensorflow. However, you cannot use dense
+     layer or dense network implementations of tf or keras. Here are some notes
+     - you can use all tf basic mathematical operations (matmul, mean or calculations etc.)
+     - you can use tf.GradientTape() to calculate gradients of the loss function
+     - you can use tf.Module and inherit your dense layer and dense network from it. Therefore, you can use all the
+       methods and arguments that the tf.Module includes (e.g, .trainable_variables)
+     - you cannot use anything else, that is connected to dense or any other layer implementation from tf
+       (e.g., tf.keras.layers.Dense)
+     - if you want to use anything else from tf, ask me, maybe I forgot to add something in this list
+"""
